2397d017bc0cbd9f810e17dd886a6582b700ce99
Ryan Lane <rlane32@gmail.com>
2016-05-25 20:11:12 -0700

    Swiftype replacement for middleman static site search (#73)
    * Replace swiftype
    
    * Add prefix to urls
    
    * Remove trailing slash from search result items

--- docs/source/javascripts/site.js
+++ docs/source/javascripts/site.js
@@ -1,11 +1,16 @@
-//= require jquery
+//=require jquery
 //=require "_vendor/highlight.pack"
 //=require "_vendor/waypoints"
 //=require "_vendor/waypoints-sticky"
-//= require anchor-js
+//=require "_vendor/underscore-min"
+//=require "_vendor/lodash.min"
+//=require anchor-js
 
 //=require "_toc"
 //=require "_docs-nav-animation"
+//=require jquery.ui.all
+//=require lunr.min
+//=require search
 
 hljs.initHighlightingOnLoad();
 anchors.add('.main h2, .main h3, .main h4, .main h5, .main h6');
