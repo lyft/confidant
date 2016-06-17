4db185c91f9be1813344a2de663a937c6f8f012f
Ryan Lane <rlane@lyft.com>
2016-06-17 16:08:56 -0700

    Update travis to build docker image for master

---
+++ docker_push.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+want_push='false'
+for branch in "master" "1.0" "1.1"
+do
+   if [ "$TRAVIS_BRANCH" == "$branch" ]
+   then
+       want_push='true'
+   fi
+done
+if [ "$TRAVIS_PULL_REQUEST" == "false" -a "$want_push" == "true" ]
+then
+    docker login -e $DOCKER_EMAIL -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+    export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
+    echo "TAG is $TAG"
+    docker tag $REPO:$COMMIT $REPO:$TAG
+    docker push $REPO:$TAG
+else
+    echo 'Ignoring PR branch for docker push.'
+fi
