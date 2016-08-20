c7142f0432c1273a92b70f7484adbf80c8dea700
Ryan Lane <rlane@lyft.com>
2016-08-20 00:56:47 -0700

    Split client out of confidant and make confidant server pip installable

--- wsgi.py
+++ wsgi.py
@@ -17,7 +17,7 @@ from confidant import routes  # noqa
 
 if __name__ == '__main__':
     app.run(
-        host='0.0.0.0',
+        host=app.config.get('HOST', '127.0.0.1'),
         port=app.config.get('PORT', 5000),
         debug=app.config.get('DEBUG', True)
     )
