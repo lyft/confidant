2d5c0293f6aec8100b97c06af965b5e5dd854a96
Ryan Lane <rlane@lyft.com>
2016-08-29 23:58:32 -0700

    Bump version for pypi fixes

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open('requirements.txt') as f:
 
 setup(
     name="confidant",
-    version="1.1.15",
+    version="1.1.16",
     packages=find_packages(exclude=["test*"]),
     package_data={
         'confidant': ['confidant/dist/*', 'requirements.txt']
