2807e2736f0a262124775012c972253116a8d3fc
Ryan Lane <rlane@lyft.com>
2016-08-30 14:33:48 -0700

    Fix docker push

--- docker_push.sh
+++ docker_push.sh
@@ -12,8 +12,8 @@ then
     docker login -e $DOCKER_EMAIL -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
     export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
     echo "TAG is $TAG"
-    docker tag $TRAVIS_REPO_SLUG:$TRAVIS_COMMIT $REPO:$TRAVIS_TAG
-    docker push $TRAVIS_REPO_SLUG:$TRAVIS_TAG
+    docker tag $TRAVIS_REPO_SLUG:$TRAVIS_COMMIT $REPO:$TAG
+    docker push $TRAVIS_REPO_SLUG:$TAG
 else
     echo 'Ignoring PR branch for docker push.'
 fi
