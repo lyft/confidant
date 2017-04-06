b747cb9a910913d0eb75efc3a7b0465729da0cca
Ryan Lane <rlane32@gmail.com>
2017-04-05 21:26:27 -0700

    Also add docker push section for master (#139)

--- docker_push.sh
+++ docker_push.sh
@@ -2,7 +2,14 @@
 if [ "$TRAVIS_PULL_REQUEST" == "false" -a -n "$TRAVIS_TAG" ]
 then
     docker login -e $DOCKER_EMAIL -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-    export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
+    export TAG="$TRAVIS_TAG"
+    echo "TAG is $TAG"
+    docker tag $TRAVIS_REPO_SLUG:$TRAVIS_COMMIT $REPO:$TAG
+    docker push $TRAVIS_REPO_SLUG:$TAG
+elif [ "$TRAVIS_PULL_REQUEST" == "false" -a "$TRAVIS_BRANCH" == "master" ]
+then
+    docker login -e $DOCKER_EMAIL -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+    export TAG="latest"
     echo "TAG is $TAG"
     docker tag $TRAVIS_REPO_SLUG:$TRAVIS_COMMIT $REPO:$TAG
     docker push $TRAVIS_REPO_SLUG:$TAG
