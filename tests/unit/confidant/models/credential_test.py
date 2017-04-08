d81a937f5a01d34dbc2fba16ddc9566bd39b3563
Ryan Lane <rlane@lyft.com>
2017-04-07 17:40:39 -0700

    Basic model tests

--- tests/unit/confidant/models/credential_test.py
+++ tests/unit/confidant/models/credential_test.py
@@ -10,10 +10,18 @@ from confidant.models.credential import EncryptError
 
 
 class CredentialTest(unittest.TestCase):
-    @patch('confidant.keymanager.get_datakey_regions', ['us-east-1'])
-    @patch('confidant.keymanager.get_datakey', 'data-key-test-data')
-    @patch('confidant.ciphermanager.CipherManager')
-    def test_encrypt_and_set_pairs(self, cipher_mock):
+    @patch(
+        'confidant.keymanager.get_datakey_regions',
+        MagicMock(return_value=['us-east-1'])
+    )
+    @patch(
+        'confidant.keymanager.create_datakey',
+        MagicMock(return_value={
+            'ciphertext': 'encrypted-data-key-test-data',
+            'plaintext': 'data-key-test-data'
+        })
+    )
+    def test_encrypt_and_set_pairs(self):
         cred = Credential(
             id='abcd1234-1',
             revision=1,
@@ -34,17 +42,24 @@ class CredentialTest(unittest.TestCase):
 
         cred.blind = False
 
-        encrypt_mock = MagicMock(
-            return_value={'us-east-1': 'encrypted_data'}
-        )
-        cipher_mock.encrypt = encrypt_mock
-        cred.encrypt_and_set_pairs({'key': 'val'}, {'id': 'abcd1234'})
+        with patch('confidant.models.credential.CipherManager') as cipher_mock:
+            encrypt_mock = MagicMock()
+            encrypt_mock.encrypt = MagicMock(
+                return_value='encrypted_data'
+            )
+            cipher_mock.return_value = encrypt_mock
+            cred.encrypt_and_set_pairs({'key': 'val'}, {'id': 'abcd1234'})
 
-        # These should be set.
-        self.assertEqual(cred.credential_pairs)
-        self.assertEqual(
-            cred.data_key,
-            {'us-east-1': base64.b64encode('data-key-test-data')}
-        )
-        self.assertEqual(cred.cipher_version, 2)
-        self.assertEqual(cred.cipher_type, 'fernet')
+            # These should be set.
+            self.assertEqual(
+                cred.credential_pairs,
+                '{"us-east-1": "encrypted_data"}'
+            )
+            self.assertEqual(
+                cred.data_key,
+                ('{"us-east-1": "' +
+                 base64.b64encode('encrypted-data-key-test-data') +
+                 '"}')
+            )
+            self.assertEqual(cred.cipher_version, 2)
+            self.assertEqual(cred.cipher_type, 'fernet')
