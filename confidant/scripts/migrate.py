526baaeec24525cc6999a5ba8a710392a222f347
Ryan Lane <rlane@lyft.com>
2017-12-01 10:39:47 -0800

    move get_save_kwargs out of class

--- confidant/scripts/migrate.py
+++ confidant/scripts/migrate.py
@@ -10,12 +10,13 @@ app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.INFO)
 
 
-class MigrateSetAttribute(Command):
+def get_save_kwargs(item):
+    return {}
 
-    def get_save_kwargs():
-        return {}
+
+class MigrateSetAttribute(Command):
 
     def run(self):
         app.logger.info('Migrating UnicodeSetAttribute in BlindCredential')
-        BlindCredential.fix_unicode_set_attributes(self.get_save_kwargs)
+        BlindCredential.fix_unicode_set_attributes(get_save_kwargs)
         app.logger.info(BlindCredential.needs_unicode_set_fix())
