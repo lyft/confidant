e37c0c38e81e1c37bc586faac97eef128d1c3717
Ryan Lane <rlane32@gmail.com>
2017-12-04 10:57:29 -0800

    Upgrade pynamo to 1.5.4 and bump version (#159)

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open('requirements.in') as f:
 
 setup(
     name="confidant",
-    version="1.10.1",
+    version="1.11.0",
     packages=find_packages(exclude=["test*"]),
     include_package_data=True,
     zip_safe=False,
