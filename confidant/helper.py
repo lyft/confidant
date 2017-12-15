826374b8f1ca7ded8a27d2aea967b52fd846e74e
Vivian Ho <vho@lyft.com>
2017-12-14 16:58:52 -0800

    added docstrings

--- confidant/helper.py
+++ confidant/helper.py
@@ -5,6 +5,15 @@ from confidant.models.service import Service
 
 
 def get_credential_context_id(cred):
+    """
+    Gets a context id for a credential (for encryption context during
+    encryption/decryption).
+
+    Arguments:
+        cred: A Credential model.
+
+    Returns: The context id as a string.
+    """
     if cred.data_type == 'credential':
         context = cred.id
     elif cred.data_type == 'archive-credential':
@@ -18,6 +27,14 @@ def get_credential_context_id(cred):
 
 
 def get_services_for_credential(_id):
+    """
+    Gets all services that use a credential.
+
+    Arguments:
+        _id: A Credential id.
+
+    Returns: A list of Services.
+    """
     services = []
     for service in Service.data_type_date_index.query('service'):
         if _id in service.credentials:
@@ -26,6 +43,14 @@ def get_services_for_credential(_id):
 
 
 def get_credential_pairs(cred):
+    """
+    Decrypts and returns all credential pairs for a credential.
+
+    Arguments:
+        cred: A Credential model.
+
+    Returns: A dictionary of credential pairs.
+    """
     context = get_credential_context_id(cred)
     data_key = keymanager.decrypt_datakey(
         cred.data_key,
