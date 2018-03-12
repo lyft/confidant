bf0aa86f4d0658f65559eae3a27fcf9d5c038fc4
Ahmed Salama <asalama@lyft.com>
2018-03-12 13:45:19 -0700

    move NonNullUnicodeSetAttribute to a seperate class

--- confidant/models/blind_credential.py
+++ confidant/models/blind_credential.py
@@ -14,25 +14,7 @@ from pynamodb.indexes import GlobalSecondaryIndex, AllProjection
 from confidant.app import app
 from confidant.models.session_cls import DDBSession
 from confidant.models.connection_cls import DDBConnection
-
-
-class NonNullUnicodeSetAttribute(UnicodeSetAttribute):
-    def __get__(self, instance, value):
-        '''
-        Override UnicodeSetAttribute's __get__ method to return a set, rather
-        than None if the attribute isn't set.
-        '''
-        if instance:
-            # Get the attribute. If the object doesn't have the attribute,
-            # ensure we return a set.
-            _value = instance.attribute_values.get(self.attr_name, set())
-            # Attribute is assigned to None, return a set instead.
-            if _value is None:
-                _value = set()
-            return _value
-        else:
-            return self
-
+from confidant.models.non_null_unicode_set_attribiute import NonNullUnicodeSetAttribute
 
 class DataTypeDateIndex(GlobalSecondaryIndex):
     class Meta:
