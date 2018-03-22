8f48eff2b747e5d8a3c3c89fac028d6e22938e60
Vivian Ho <vho@lyft.com>
2018-03-21 20:13:36 -0700

    remove very verbose log line

--- confidant/routes/v1.py
+++ confidant/routes/v1.py
@@ -117,9 +117,6 @@ def get_service(id):
             msg = 'Authenticated user is not authorized.'
             return jsonify({'error': msg}), 401
     except DoesNotExist:
-        logging.warning(
-            'Item with id {0} does not exist.'.format(id)
-        )
         return jsonify({}), 404
     if (service.data_type != 'service' and
             service.data_type != 'archive-service'):
