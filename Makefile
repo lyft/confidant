2990bbf91022ba75ff2935275bbc3651bc61c25e
Ryan Lane <rlane@lyft.com>
2020-01-03 13:49:35 -0800

    Switch deps for pytest and move environment setup

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ test: test_unit
 
 test_unit:
 	mkdir -p build
-	nosetests --with-coverage --with-path=confidant tests/unit
+	pytest --strict --junitxml=build/unit.xml --cov=confidant --cov-report=html --cov-report=xml --no-cov-on-fail tests/unit
 
 .PHONY: compile_deps # freeze requirements.in to requirements3.txt
 compile_deps:
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,25 +1,5 @@
-# Make the tests directory a python module
-# so that all unit tests are reported as part of the tests package.
+# Load before anything to prevent infinite loop on requests
+#   See: https://github.com/gevent/gevent/issues/941
+import gevent.monkey
 
-import os
-
-# Inject mandatory environment variables
-
-env_settings = [
-    ('SESSION_SECRET', 'secret'),
-    ('DYNAMODB_TABLE', 'confidant-testing'),
-    ('DYNAMODB_URL', 'http://dynamo:7777'),
-    ('DYNAMODB_CREATE_TABLE', 'false'),
-    ('GEVENT_RESOLVER', 'ares'),
-    ('AWS_DEFAULT_REGION', 'us-east-1'),
-    ('USER_AUTH_KEY', 'authnz-usertesting'),
-    ('AUTH_KEY', 'authnz-testing'),
-    ('SCOPED_AUTH_KEYS',
-     '{"sandbox-auth-key":"sandbox","primary-auth-key":"primary"}'),
-    ('KMS_MASTER_KEY', 'confidant-mastertesting'),
-    ('DEBUG', 'true'),
-    ('STATIC_FOLDER', 'public')
-]
-
-for env_setting in env_settings:
-    os.environ[env_setting[0]] = os.getenv(env_setting[0], env_setting[1])
+gevent.monkey.patch_all()
