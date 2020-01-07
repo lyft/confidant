5a4dcf2c44ef563d874ba957f993e05076208ee8
Ryan Lane <rlane@lyft.com>
2020-01-06 17:49:34 -0800

    Fix integration tests too

--- tests/integration/confidant/authnz/authnz_test.py
+++ tests/integration/confidant/authnz/authnz_test.py
@@ -3,6 +3,7 @@ import base64
 from mock import patch
 from mock import PropertyMock
 
+from confidant import settings
 from confidant.wsgi import app
 from confidant.authnz import userauth
 from confidant import routes  # noqa
@@ -12,27 +13,27 @@ class AuthnzTest(unittest.TestCase):
 
     def setUp(self):
         self.test_client = app.test_client()
-        self.email_suffix = app.config['USER_EMAIL_SUFFIX']
-        app.config['USER_EMAIL_SUFFIX'] = ''
-        self.use_auth = app.config['USE_AUTH']
-        self.users_file = app.config['USERS_FILE']
+        self.email_suffix = settings.USER_EMAIL_SUFFIX
+        settings.USER_EMAIL_SUFFIX = ''
+        self.use_auth = settings.USE_AUTH
+        self.users_file = settings.USERS_FILE
         self.debug = app.debug
 
     def tearDown(self):
-        app.config['USER_EMAIL_SUFFIX'] = self.email_suffix
-        app.config['USE_AUTH'] = self.use_auth
-        app.config['USERS_FILE'] = self.users_file
+        settings.USER_EMAIL_SUFFIX = self.email_suffix
+        settings.USE_AUTH = self.use_auth
+        settings.USERS_FILE = self.users_file
         app.debug = self.debug
 
     def test_auth_redirect(self):
         app.debug = True
-        app.config['USE_AUTH'] = True
+        settings.USE_AUTH = True
         ret = self.test_client.get('/', follow_redirects=False)
         self.assertEquals(ret.status_code, 302)
 
     def test_no_auth(self):
         app.debug = True
-        app.config['USE_AUTH'] = False
+        settings.USE_AUTH = False
         user_mod = userauth.init_user_auth_class()
         with patch('confidant.authnz.user_mod', user_mod):
             ret = self.test_client.get('/v1/user/email')
@@ -40,15 +41,15 @@ class AuthnzTest(unittest.TestCase):
 
     def test_auth_failure(self):
         app.debug = True
-        app.config['USE_AUTH'] = True
+        settings.USE_AUTH = True
         ret = self.test_client.get('/v1/user/email', follow_redirects=False)
         self.assertEquals(ret.status_code, 401)
 
     def test_auth_with_email_session_in_users(self):
         app.debug = True
-        app.config['USE_AUTH'] = True
+        settings.USE_AUTH = True
         # USERS_FILE needs to be set to test users file
-        app.config['USERS_FILE'] = ''
+        settings.USERS_FILE = ''
         to_patch = ('confidant.authnz.userauth.AbstractUserAuthenticator.'
                     'allowed_email_whitelist')
         with patch(to_patch, new_callable=PropertyMock) as mock_whitelist:
@@ -64,9 +65,9 @@ class AuthnzTest(unittest.TestCase):
 
     def test_auth_with_email_session_not_in_users(self):
         app.debug = True
-        app.config['USE_AUTH'] = True
+        settings.USE_AUTH = True
         # USERS_FILE needs to be set to test users file
-        app.config['USERS_FILE'] = ''
+        settings.USERS_FILE = ''
         to_patch = ('confidant.authnz.userauth.AbstractUserAuthenticator.'
                     'allowed_email_whitelist')
         with patch(to_patch, new_callable=PropertyMock) as mock_whitelist:
@@ -83,8 +84,8 @@ class AuthnzTest(unittest.TestCase):
     def test_auth_with_email_session(self):
         app.debug = True
         # Unset the USERS_FILE, in case it's been set elsewhere
-        app.config['USERS_FILE'] = ''
-        app.config['USE_AUTH'] = True
+        settings.USERS_FILE = ''
+        settings.USE_AUTH = True
         to_patch = ('confidant.authnz.userauth.AbstractUserAuthenticator.'
                     'allowed_email_whitelist')
         with patch(to_patch, new_callable=PropertyMock) as mock_whitelist:
@@ -100,11 +101,11 @@ class AuthnzTest(unittest.TestCase):
 
     def test_header_csrf(self):
         app.debug = True
-        app.config['USE_AUTH'] = True
-        app.config['USER_AUTH_MODULE'] = 'header'
-        app.config['HEADER_AUTH_USERNAME_HEADER'] = 'X-Confidant-User'
-        app.config['HEADER_AUTH_EMAIL_HEADER'] = 'X-Confidant-Email'
-        app.config['XSRF_COOKIE_NAME'] = 'XSRF-TOKEN'
+        settings.USE_AUTH = True
+        settings.USER_AUTH_MODULE = 'header'
+        settings.HEADER_AUTH_USERNAME_HEADER = 'X-Confidant-User'
+        settings.HEADER_AUTH_EMAIL_HEADER = 'X-Confidant-Email'
+        settings.XSRF_COOKIE_NAME = 'XSRF-TOKEN'
         user_mod = userauth.init_user_auth_class()
         with patch('confidant.authnz.user_mod', user_mod):
             ret = self.test_client.get(
@@ -128,7 +129,7 @@ class AuthnzTest(unittest.TestCase):
 
     def test_invalid_kms_auth_token(self):
         app.debug = True
-        app.config['USE_AUTH'] = True
+        settings.USE_AUTH = True
         auth = base64.b64encode(b'confidant-development:faketoken').decode()
         ret = self.test_client.get(
             '/v1/services/confidant-development',
