9c9756e4852f15e43ca2dbe4a2b4ad19ccfdfb7e
Ryan Lane <rlane@lyft.com>
2020-01-06 17:53:45 -0800

    Fix dev_wsgi.py

--- dev_wsgi.py
+++ dev_wsgi.py
@@ -1,9 +1,10 @@
+from confidant import settings
 from confidant.app import create_app
 
 if __name__ == '__main__':
     app = create_app()
     app.run(
-        host=app.config.get('HOST', '127.0.0.1'),
-        port=app.config.get('PORT', 5000),
-        debug=app.config.get('DEBUG', True)
+        host=settings.get('HOST', '127.0.0.1'),
+        port=settings.get('PORT', 5000),
+        debug=settings.get('DEBUG', True)
     )
