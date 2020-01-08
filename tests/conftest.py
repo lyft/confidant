a04e48472ab3c11f52bc293a01c13ce94fcb08ae
Ryan Lane <rlane@lyft.com>
2020-01-07 19:44:37 -0800

    Do not import settings in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,10 @@
 import pytest
 
-from confidant import settings
-
 
 @pytest.fixture(autouse=True)
-def encrypted_settings_mock():
-    settings.encrypted_settings.secret_string = {}
-    settings.encrypted_settings.decrypted_secrets = {
-        'SESSION_SECRET': 'TEST_KEY'
-    }
+def encrypted_settings_mock(mocker):
+    mocker.patch('confidant.settings.encrypted_settings.secret_string', {})
+    mocker.patch(
+        'confidant.settings.encrypted_settings.decrypted_secrets',
+        {'SESSION_SECRET': 'TEST_KEY'},
+    )
