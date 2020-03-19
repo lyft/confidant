46a1e794fd15aa84669b01a5614887310fe2b376
irhkang <irhkang@users.noreply.github.com>
2020-03-19 22:14:08 +0000

    Deploying to gh-pages from master - 373c0a60cd5b146c80c19eea93c5e42574fb1ad9 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '6.3.0-4caa49',
+    VERSION: '6.4.0-373c0a',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
