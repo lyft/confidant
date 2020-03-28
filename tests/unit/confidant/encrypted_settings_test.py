9baebaead1a3ec9af7e696139e7b7c1a50292d2e
Ryan Lane <rlane@lyft.com>
2020-03-27 18:22:29 -0700

    Test fixes for EncryptedSettings

--- tests/unit/confidant/encrypted_settings_test.py
+++ tests/unit/confidant/encrypted_settings_test.py
@@ -2,20 +2,20 @@ from confidant.encrypted_settings import EncryptedSettings
 
 
 def test_register():
-    enc_set = EncryptedSettings(None)
+    enc_set = EncryptedSettings(None, None)
     enc_set.register('Foo', 'Bar')
     assert enc_set.secret_names == ['Foo']
 
 
 def test_get_registered():
-    enc_set = EncryptedSettings(None)
+    enc_set = EncryptedSettings(None, None)
     enc_set.register('Foo', 'Bar')
     enc_set.decrypted_secrets = {'Foo': 'DecryptedBar'}
     assert enc_set.get_secret('Foo') == 'DecryptedBar'
 
 
 def test_get_registered_default():
-    enc_set = EncryptedSettings(None)
+    enc_set = EncryptedSettings(None, None)
     enc_set.register('Foo', 'Bar')
     enc_set.register('Bar', 'Baz')
     enc_set.decrypted_secrets = {'Foo': 'DecryptedFoo'}
@@ -31,7 +31,7 @@ def test_bootstrap(mocker):
         'confidant.encrypted_settings.Fernet.decrypt',
         return_value='{secret: value, secret2: value2}\n',
     )
-    enc_set = EncryptedSettings(None)
+    enc_set = EncryptedSettings(None, None)
     decrypted = enc_set._bootstrap(
         '{"secrets": "encryptedstring", "data_key": "dGhla2V5"}'
     )
@@ -39,6 +39,6 @@ def test_bootstrap(mocker):
 
 
 def test_bootstrap_filefail():
-    enc_set = EncryptedSettings(None)
+    enc_set = EncryptedSettings(None, None)
     decrypted = enc_set._bootstrap('file://FILE/DOES/NOT/EXIST')
     assert decrypted == {}
