78afac4c877f8dc14c68677408f075f889dae44d
skiptomyliu <deanliu@gmail.com>
2021-05-12 03:35:58 +0000

    Deploying to gh-pages from master @ def0f33ab25617e348c260204e4e0845eef617c2 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '6.3.0-197ca2',
+    VERSION: '6.3.0-def0f3',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
