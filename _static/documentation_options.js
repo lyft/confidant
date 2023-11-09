8beb9f3eb134357a4bf46d65a73e2db511b5e5de
leifrf <leifrf@users.noreply.github.com>
2023-11-09 00:41:50 +0000

    Deploying to gh-pages from master @ 7236b9055eefded18c0cef4aeb2d38be2d6836aa 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '6.5.1-7c5743',
+    VERSION: '6.5.1-7236b9',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
