65571a5dd5fba825e0cd2516a13bd6d225cb24e2
Alejandro Roiz Walss <alejandroroiz@lyft.com>
2024-08-13 10:36:21 -0600

    revert escape values

--- tests/unit/confidant/routes/identity_test.py
+++ tests/unit/confidant/routes/identity_test.py
@@ -63,7 +63,7 @@ def test_get_client_config(mocker: MockerFixture):
             'xsrf_cookie_name': 'CSRF_TOKEN',
             'maintenance_mode': True,
             'history_page_limit': 50,
-            'defined_tags': [],
+            'defined_tags': ['ROTATION_EXCLUDED', 'FINANCIALLY_SENSITIVE'],
             'permissions': {
                 'credentials': {
                     'list': True,
