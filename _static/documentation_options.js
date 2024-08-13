0d7515921b93563091ac8eff8d84d3c752ac5519
alejandroroiz <26120069+alejandroroiz@users.noreply.github.com>
2024-08-13 18:15:08 +0000

    Deploying to gh-pages from master @ be4b0ee33ea80e43382584bddd3c0385ab171a76 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '6.6.1-4381e2',
+    VERSION: '6.6.1-be4b0e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
