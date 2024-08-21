c9f66a88b4ef6b11587e8bb5e52ccdb2a9bb42a9
meng-han <4225835+meng-han@users.noreply.github.com>
2024-08-21 16:02:25 +0000

    Deploying to gh-pages from master @ 6328f138611ea1177c6277010f15b56ed0500cac 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '6.6.1-b8d4bd',
+    VERSION: '6.6.1-6328f1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
