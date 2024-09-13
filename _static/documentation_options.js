240f4d95a56a902980902a16c19a61ce48155911
whu-lyft <173964158+whu-lyft@users.noreply.github.com>
2024-09-13 16:32:37 +0000

    Deploying to gh-pages from master @ 8876b07abde0c8d2a4974f79b60562b6d0193db9 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '6.6.1-ef1d61',
+    VERSION: '6.6.2-8876b0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
