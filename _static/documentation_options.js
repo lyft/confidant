c21bb33db7e794ec592979de167a602cec2f8f9c
whu-lyft <173964158+whu-lyft@users.noreply.github.com>
2025-02-14 22:53:35 +0000

    Deploying to gh-pages from master @ 9f5c36ff389241d959e62c69879726339d629be0 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '6.6.2-1b4b1c',
+    VERSION: '7.0.0-9f5c36',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
